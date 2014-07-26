fc8cdc8d26b57646163f3a23817158adf3708846
Sebastian Golasch <public@asciidisco.com>
2014-07-26 18:47:48 +0200

    Added pointer.doubleclick method

---
+++ src/plugins/pointer/pointer.doubleclick.js
@@ -0,0 +1,8 @@
+// Status: NEW
+// Category: Cursor
+// Docs: -
+// Source: -
+// WIRE: https://code.google.com/p/selenium/wiki/JsonWireProtocol#POST_/session/:sessionId/doubleclick
+
+pointer.doubleClick('#faq')
+
