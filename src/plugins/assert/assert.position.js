c9f77289e5e15860d1c7f6747f193072041745b6
Sebastian Golasch <public@asciidisco.com>
2014-07-26 18:53:48 +0200

    Added element position assertion

---
+++ src/plugins/assert/assert.position.js
@@ -0,0 +1,11 @@
+// Status: NEW
+// Docs: -
+// Source: -
+// WIRE: https://code.google.com/p/selenium/wiki/JsonWireProtocol#GET_/session/:sessionId/element/:id/location
+
+assert.position({
+  selector: '#foo bar',
+  expected: [x, y], // Coordinates in pixels from the left-upper corner of the page
+  threshold: [x1, x2, y1, y2], // MAYBE define a threshold instead of mostly useless exact values
+  message: 'Element is positioned within a specific threshold'
+})
