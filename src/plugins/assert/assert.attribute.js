cc9f01065d9c1d5942be12035ff59b990b4a47a3
Rodney Rehm <mail+github@rodneyrehm.de>
2014-07-27 18:54:58 +0200

    adding required properties to meta

--- src/plugins/assert/assert.attribute.js
+++ src/plugins/assert/assert.attribute.js
@@ -23,6 +23,8 @@ module.exports = function(dalek) {
   var meta = {
     // allow calls like assert.attribute('.some-thing', 'attr-name', 'expected-value')
     signature: ['selector', 'name', 'expected'],
+    // list of properties that must be specified at the very least
+    required: ['selector', 'name', 'expected'],
     // mark this plugin capable of handling {match: "all"}
     iterator: true,
     // mark this plugin as capable of handling .not.attribute()
