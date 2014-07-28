8a6741eccb8e99e55e2335f9c2b419426a8b9f99
Rodney Rehm <mail+github@rodneyrehm.de>
2014-07-28 11:28:06 +0200

    run stuff in a domain for convenient access to errors (amongst other things)

--- index.js
+++ index.js
@@ -6,18 +6,41 @@ var Dalek = require('./src/Dalek');
 console.log('----- starting dalek');
 var dalek = new Dalek();
 
-// this is what happens inside a unit test definition
-console.log('----- registering attribute assertion');
-//var called = dalek.assert.attribute('some-selector', 'attribute-name', 'Google');
-var called = dalek.assert.not.attribute('some-selector', 'attribute-name', function(value){ return value === 'Apple'; });
+var domain = require('domain').create();
+domain.on('error', function(error) {
+  dalek.reporter.error(error);
+});
 
-// this is what happens when the unit test is executed
-console.log('----- running attribute assertion in test');
-var assertionInstance = called();
+domain.run(function() {
 
-// this is what is returned to the unit test
-console.log('assertion', assertionInstance.label);
-assertionInstance.then(
-  function(message){ console.log("SUCCESS", message); },
-  function(message){ console.log("ERROR", message); }
-);
+  // triggering DalekErrors with custom stack
+  // throw new dalek.Error('foobar', dalek.Error.PLUGIN_CALL, [
+  //   {name: 'someFunction', file: 'index.js', line: 123}
+  // ]);
+
+  // this is what happens inside a unit test definition
+  console.log('----- registering attribute assertion');
+
+  // literal example
+  // var called = dalek.assert.attribute('some-selector', 'attribute-name', 'Google');
+
+  // custom function example
+  var comp = function(value){ return value === 'Apple' ? '' : 'bla bla'; };
+  comp.displayName = 'deine mudder';
+  var called = dalek.assert.not.attribute('some-selector', 'attribute-name', comp);
+
+  // using is-plugin example
+  // var called = dalek.assert.not.attribute('some-selector', 'attribute-name', is.equal('foobar'));
+
+  // this is what happens when the unit test is executed
+  console.log('----- running attribute assertion in test');
+  var assertionInstance = called();
+
+  // this is what is returned to the unit test
+  console.log('assertion', assertionInstance.label);
+  assertionInstance.then(
+    function(message){ console.log("SUCCESS", message); },
+    function(message){ console.log("ERROR", message); }
+  );
+
+});
