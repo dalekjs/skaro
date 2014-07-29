0f05c69fc543928332b190be0ff958b3a233eac9
Rodney Rehm <mail+github@rodneyrehm.de>
2014-07-29 20:18:23 +0200

    managing suites

--- src/core/Suite.js
+++ src/core/Suite.js
@@ -2,7 +2,6 @@ module.exports = function(dalek) {
   'use strict';
 
   var _ = dalek._;
-  var Q = dalek.Q;
 
   function Suite(label, options, callback, called) {
     this.label = label;
@@ -21,10 +20,12 @@ module.exports = function(dalek) {
     this._units = [];
     this._beforeUnit = null;
     this._afterUnit = null;
+
+    this._runLoop = this._runLoop.bind(this);
   }
 
-  Suite.prototype.options = function(options) {
-    if (typeof options === undefined) {
+  Suite.prototype.options = function(options, defaultValue) {
+    if (options === undefined) {
       return _.clone(this._options);
     }
 
@@ -36,6 +37,7 @@ module.exports = function(dalek) {
   };
 
   Suite.prototype.initialize = function() {
+    dalek.reporter.debug('initializing suite', this.label);
     this._initialize(this, this.options);
 
     if (!this._units.length) {
@@ -46,7 +48,11 @@ module.exports = function(dalek) {
       );
     }
 
-    dalek.reporter.debug('initialized suite', this.label);
+    this.handle = new dalek.Handle(this.label, dalek.Handle.SUITE);
+    this.handle.children = this._units.length;
+    this.handle.operations = 0;
+
+    return this.handle;
   };
 
   Suite.prototype.unit = function(label, callback) {
@@ -68,8 +74,67 @@ module.exports = function(dalek) {
     this._afterUnit = new dalek.Unit('after:suite', this.options(), callback, called);
   };
 
+  Suite.prototype.runBeforeUnit = function() {
+    return this.runUnitWrapper(this._beforeUnit);
+  };
+
+  Suite.prototype.runAfterUnit = function() {
+    return this.runUnitWrapper(this._afterUnit);
+  };
+
+  Suite.prototype.runUnitWrapper = function(unit) {
+    if (!unit) {
+      return dalek.Q();
+    }
+
+    var unitHandle = unit.initialize();
+    unit.run();
+
+    // report only in failure case
+    unitHandle.catch(function(failure) {
+      dalek.reporter.started(unitHandle);
+      dalek.reporter.failed(unitHandle, failure);
+      this.handle.reject(unitHandle);
+      throw failure;
+    }.bind(this));
+
+    return unitHandle;
+  };
+
   Suite.prototype.run = function() {
     dalek.reporter.debug('Running Suite', this.label);
+    this._runLoop();
+    return this.handle;
+  };
+
+  Suite.prototype._runLoop = function() {
+    var unit = this._units.shift();
+    if (!unit) {
+      this.handle.resolve();
+      return;
+    }
+
+    var unitHandle = unit.initialize();
+    this.handle.operations++;
+
+    this.runBeforeUnit().then(function() {
+      unit.run();
+      dalek.reporter.started(unitHandle);
+    }.bind(this));
+
+    var success = function() {
+      dalek.reporter.succeeded(unitHandle);
+      this.runAfterUnit().then(this._runLoop);
+    }.bind(this);
+
+    var failure = function(failure) {
+      dalek.reporter.failed(unitHandle, failure);
+      this.runAfterUnit().then(this.handle.reject);
+    }.bind(this);
+
+    unitHandle
+      .then(success, failure)
+      .catch(dalek.catch);
   };
 
   return Suite;
