31286aedf781db60b6f5b093c98b81740afac3e0
Rodney Rehm <mail+github@rodneyrehm.de>
2014-07-30 21:20:26 +0200

    adding CLI handling

---
+++ bin/dalek.cli.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+'use strict';
+
+// https://github.com/harthur/nomnom
+
+var cliOptions = require('./dalek.cli.options.js')();
+var Config = require('../src/core/Config');
+var config = new Config(cliOptions, process.cwd());
+config.verify();
+
+var Dalek = require('../index.js');
+var dalek = new Dalek(config);
+
+// TODO: move the domain thing to dalek!
+var domain = require('domain').create();
+domain.on('error', function(error) {
+  dalek.reporter.error(error);
+});
+domain.run(function() {
+  // TODO: glob() from dalek.options('test');
+  require('../example/tests/Dummy')(dalek);
+
+  // TODO: start and stop a test run properly
+  dalek.run().then(function() {
+    console.log('\nEXTERMINATED');
+  }, function(/*failedSuiteHandle*/) {
+    console.log('\nDALEK FAILED');
+  });
+});
---
+++ bin/dalek.cli.options.js
@@ -0,0 +1,113 @@
+module.exports = function() {
+  'use strict';
+
+  var nomnom = require('nomnom').script('dalek');
+
+  // global options
+  nomnom.options({
+    // configuration file and options
+    config: {
+      abbr: 'c',
+      metavar: 'FILE',
+      // TODO: windows compatible path
+      default: 'Dalekfile.json',
+      help: 'path to configuration file (disable with --no-config)'
+    },
+    option: {
+      abbr: 'o',
+      type: 'string',
+      list: true,
+      metavar: 'name=val',
+      help: 'config option(s) to set'
+    },
+
+    // console output
+    debug: {
+      flag: true,
+      help: 'engange debug mode'
+    },
+    silent: {
+      flag: true,
+      help: 'suppress all output'
+    },
+    noColors: {
+      full: 'no-colors',
+      flag: true,
+      help: 'disable colorized output in the console'
+    },
+    noSymbols: {
+      full: 'no-symbols',
+      flag: true,
+      help: 'disable UTF-8 symbols in the console'
+    },
+  });
+
+  // local options
+  nomnom.nocommand().options({
+    version: {
+      abbr: 'v',
+      flag: true,
+      help: 'print version and exit',
+      callback: function() {
+        return 'version ' + require('../package.json').version;
+      }
+    },
+
+    // plugins
+    plugin: {
+      type: 'string',
+      list: true,
+      help: 'plugin file(s) to load'
+    },
+
+    // tests
+    test: {
+      type: 'string',
+      list: true,
+      help: 'test file(s) to load'
+    },
+    ignore: {
+      type: 'string',
+      list: true,
+      help: 'test file(s) not to load'
+    },
+
+    // affecting browsers
+    browser: {
+      abbr: 'b',
+      type: 'string',
+      list: true,
+      help: 'Browser(s) you would like to invoke'
+    },
+    viewport: {
+      type: 'string',
+      help: 'dimensions of the browser viewport at startup (shorthand for -o viewport=<val>)'
+    },
+
+    // affecting reporting
+    reporter: {
+      abbr: 'r',
+      type: 'string',
+      list: true,
+      help: 'Reporter(s) to spin up for logging test results',
+      choices: ['console', 'html', 'json', 'junit']
+    },
+  }).help('See the docs for the CLI at http://dalekjs.com/docs/cli.html');
+
+  // remote-proxy options
+  nomnom.command('remote').options({
+    'remote.port': {
+      full: 'port',
+      default: 9020,
+      help: 'port to bind to'
+    },
+    'remote.interface': {
+      full: 'interface',
+      type: 'text',
+      default: 'localhost',
+      help: 'network interface to bind to'
+    }
+  }).help('Start Dalek as a remote proxy');
+
+  return nomnom.parse();
+};
