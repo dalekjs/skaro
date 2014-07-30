1405441309315826e639cdb330c74258b4b47699
Rodney Rehm <mail+github@rodneyrehm.de>
2014-07-30 14:39:41 +0200

    parse error stack

--- src/util/getStack.js
+++ src/util/getStack.js
@@ -4,10 +4,10 @@ var stackTrace = require('stack-trace');
 
 // extract the useful (developer-relevant) parts of the call-stack
 // to present in case of problems during plugin call and exection-time
-module.exports = function getStack(below) {
+module.exports = function getStack(below, error) {
   // https://github.com/felixge/node-stack-trace
   var stack = [];
-  var trace = stackTrace.get(below);
+  var trace = error && stackTrace.parse(error) || stackTrace.get(below);
 
   trace.some(function(callSite) {
     var site = {
@@ -17,7 +17,7 @@ module.exports = function getStack(below) {
     };
 
     // we don't care about anything deeper than this
-    if (site.name === 'Module._compile') {
+    if (site.name === 'Module._compile' || site.name === 'listOnTimeout') {
       return true;
     }
 
