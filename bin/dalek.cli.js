f32a6a75a54acbee5d77d1efe3443861d76ddc34
Rodney Rehm <mail+github@rodneyrehm.de>
2014-08-03 13:32:52 +0200

    properly format cli usage suggestion

--- bin/dalek.cli.js
+++ bin/dalek.cli.js
@@ -2,6 +2,7 @@
 'use strict';
 
 var chalk = require('chalk');
+var wordwrap = require('wordwrap');
 var cli = require('./dalek.cli.options.js')();
 
 if (cli.options.version) {
@@ -19,16 +20,18 @@ var config = new Config(cli.options, cli.files, process.cwd());
 config.load().then(function(config) {
   require('./commands/command.' + cli.command)(config, cli);
 }).catch(function(reason) {
-
+  var wrap = wordwrap(80);
   if (reason === Config.FILE_NOT_FOUND) {
     cli.error(
-      'Configuration file (' + cli.options.config + ') not found',
-      'Are you running dalek in the correct directory?\n'
-      + 'You can reference a configuration file from anywhere using '
-      + chalk.blue('--config=path/to/Dalekfile.json') + ' or skip the config file with '
-      + chalk.blue('--no-config') + '. In both cases configuration options can be provided using '
-      + chalk.blue('--option option.name=option.value') + '.'
-    );
+      'Configuration file (' + cli.options.config + ') not found', wrap(
+        /*jshint laxbreak:true */
+        'Are you running dalek in the correct directory?\n\n'
+        + 'You can reference a configuration file from anywhere using '
+        + chalk.blue('--config=path/to/Dalekfile.json') + ' or skip the config file with '
+        + chalk.blue('--no-config') + '. In both cases configuration options can be provided using '
+        + chalk.blue('--option option.name=option.value') + '.'
+        /*jshint laxbreak:false */
+    ));
     return;
   }
 
