3a36f07f563c5a479b52f96e20fc2a5819ad2c78
Rodney Rehm <mail+github@rodneyrehm.de>
2014-08-03 10:28:30 +0200

    replacing child process with find installed packages util

--- bin/dalek.cli.version.js
+++ bin/dalek.cli.version.js
@@ -1,55 +1,29 @@
 module.exports = function() {
   'use strict';
 
-  var childProcess = require('child_process');
   var chalk = require('chalk');
-  var Q = require('q');
+  var findInstalledPackages = require('../src/util/find-installed-packages');
   var packageMeta = require('../package.json');
 
-  function executeToJSON(command) {
-    var deferred = Q.defer();
-    childProcess.exec(command, function(err, stdout/*, stderr*/) {
-      // need to disable error handling, because npm is trying to be over-helpful here:
-      // npm ERR! extraneous: matchdep@0.3.0 /Users/rrehm/Projekte/test.dev/htdocs/dalekjs/skaro/node_modules/matchdep
-      // npm ERR! not ok code 0 
-      // if (err) {
-      //   deferred.reject(err);
-      // }
-
-      deferred.resolve(JSON.parse(stdout));
-    });
-
-    return deferred.promise;
+  function filterDalek(packageName) {
+    return packageName.slice(0, 5) === 'dalek';
   }
-
-  function filterDalek(installed) {
-    return Object.keys(installed.dependencies).filter(function(key) {
-      return key.slice(0, 6) === 'dalek-';
-    }).map(function(key) {
-      return '    ' + chalk.bgBlack.cyan(key) + ' ' + chalk.bgBlack.yellow('@' + installed.dependencies[key].version);
-    }).join('\n');
-  }
-
 
   console.log('\n' + chalk.bgBlack.cyan(packageMeta.name), 'version', chalk.bgBlack.yellow(packageMeta.version) + '\n');
   if (packageMeta.description) {
     console.log(packageMeta.description + '\n');
   }
 
-  Q.all([
-    // globally installed packages
-    executeToJSON('npm ls -g --json --depth 0').then(filterDalek),
-    // locally installed packages
-    executeToJSON('npm ls --json --depth 0').then(filterDalek),
-  ]).spread(function (_global, _local) {
+  findInstalledPackages(filterDalek).then(function(packages) {
+    console.log('  locally installed Dalek packages');
+    packages.local.forEach(function(item) {
+      console.log('    ' + chalk.bgBlack.cyan(item.name) + ' ' + chalk.bgBlack.yellow('@' + item.version));
+    });
 
-    // TODO: add WD.js version to output
-
-    console.log('  globally installed Dalek packages');
-    console.log(_global);
-
-    console.log('\n  locally installed Dalek packages');
-    console.log(_local);
+    console.log('\n  globally installed Dalek packages');
+    packages.global.forEach(function(item) {
+      console.log('    ' + chalk.bgBlack.cyan(item.name) + ' ' + chalk.bgBlack.yellow('@' + item.version) + ' in ' + item.__directory);
+    });
   }).done();
 
 };
