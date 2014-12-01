6f801f91c55dd98771312cdcd227291f28b9611a
Sebastian Golasch <public@asciidisco.com>
2014-12-01 10:49:17 +0100

    chore(driver): removed some old cruft

--- src/core/Driver.js
+++ src/core/Driver.js
@@ -4,67 +4,16 @@ module.exports = function(dalek) {
   var WD = require('wd');
 
   // TODO: create driver interface
-  function Driver(options) {
+  function Driver (options) {
     this.options = options;
   };
 
   // initialize WD.js
   Driver.prototype.initializeWebdriverConnection = function (options) {
     this.wd = WD.promiseChainRemote('http://' + options.host + ':' + options.port + '/');
-    // TODO: I don't like this, assigning the WD instance to every "category"
-    this.element.wd = this.wd;
 
     // initializa (aka. get the session id)
     return this.wd.init({browserName: options.id});
-  };
-
-
-  Driver.prototype.element = {
-    attribute: function(/*options*/) {
-      /*
-          options: {
-            selector: Selector
-            match: String
-            attribute: String
-          }
-
-          success case (returns array):
-            resolve(['value of element1', 'value of element2', …])
-
-          error cases:
-            reject('Selector did not match any elements')
-            reject(new DalekError('browser unable to execute selector'))
-            reject(new DalekError('lost connection to browser', DalekError.NETWORK))
-            reject(new DalekError('request timed out', DalekError.TIMEOUT))
-            reject(new DalekError('Chrome driver does not support fetching attributes', DalekError.DRIVER))
-       */
-      var deferred = dalek.Q.defer();
-      deferred.resolve(['Google', 'Microsoft']);
-      return deferred.promise;
-    },
-
-    click: function(/*options*/) {
-      /*
-          options: {
-            selector: Selector
-          }
-
-          success case (returns array):
-            resolve([true, true, true, …])
-
-          error cases:
-            resolve([true, 'element not found', 'element not visible', …])
-            reject('Selector did not match any elements')
-            reject(new DalekError('browser unable to execute selector'))
-            reject(new DalekError('lost connection to browser', DalekError.NETWORK))
-            reject(new DalekError('request timed out', DalekError.TIMEOUT))
-            reject(new DalekError('Firefox driver does not support the click method', DalekError.DRIVER))
-       */
-      var deferred = dalek.Q.defer();
-      // deferred.resolve([true, 'element not found']);
-      deferred.resolve([true, true]);
-      return deferred.promise;
-    }
   };
 
   return Driver;
