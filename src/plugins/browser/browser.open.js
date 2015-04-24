afd1223a31430aebe9378e432bc3e9458b038361
Rodney Rehm <mail+github@rodneyrehm.de>
2015-04-24 18:10:17 +0200

    feature(plugin.browser.open): verify target url to fail the action when a resource is not resolvable

--- src/plugins/browser/browser.open.js
+++ src/plugins/browser/browser.open.js
@@ -44,41 +44,37 @@ module.exports = function(dalek) {
     namespace: 'browser',
     // name of the plugin
     name: 'open',
-    // allow calls like assert.click('.some-thing')
+    // allow calls like browser.open('.some-thing')
     signature: ['url'],
     // list of properties that must be specified at the very least
     required: ['url']
   };
 
   var handler = function(options) {
-    // the name invocations of this plugin will show up as
-    var label = format.keyword(meta.name) + ' url ' + format.literal(options.url);
-
+    // human readable name of what is supposed to happen
+    var label = 'open url ' + format.literal(options.url);
     // we're creating an action, give dalek that context
-    var handle = new dalek.Handle(label, dalek.Handle.BROWSER, meta.name);
+    var handle = new dalek.Handle(label, dalek.Handle.ACTION, meta.name);
 
-    // data we need to pass to WebDriver
-    var data = {
-      url: options.url
-    };
-
-    var handleResults = function(err) {
-      if (!!err) {
-          handle.rejectWithMessage(err, options.message, result);
-          return true;
+    var handleResponse = function(url) {
+      // if a domain could not be resolved, browser's default to about:blank
+      if (!url || (url === 'about:blank' && url !== options.url)) {
+        handle.reject('could not open url, saw ' + format.literal(url) + ' instead');
+        return;
       }
 
-      handle.resolveNavigation(options.url);
+      handle.resolve('opened ' + format.literal(url));
     };
 
-    // talk to WebDriver
-    dalek.wd.get(options.url).then(
-      // process WebDriver results
-      handleResults,
-      // WebDriver rejects on empty selector-result with string
-      // any errors (including malformed selector) with DalekError
-      handle.reject
-    ).catch(dalek.catch);
+    dalek.wd
+      // open URL
+      .get(options.url)
+      // query open URL
+      .url()
+      // verify we're somewhere
+      .then(handleResponse, handle.reject)
+      // instead of .done() we inform dalek when something went terribly wrong
+      .catch(dalek.catch);
 
     return handle;
   };
