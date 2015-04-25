981a9d19eec614a7352b438380cad1154220dd1a
Rodney Rehm <mail+github@rodneyrehm.de>
2015-04-25 20:13:43 +0200

    feature(plugin.assert.selected): implementing selected assertion

--- src/plugins/assert/assert.selected.js
+++ src/plugins/assert/assert.selected.js
@@ -3,16 +3,16 @@
   category: 'form'
 }
 
-# Name Of Plugin
+# Assert Selected
 
-Description Of Plugin
+Assert elements are selected (`<option>`) or checked (`<input type="checkbox|radio">`).
 
 
 ## Examples
 
 ```js
-assert.selected()
-assert.not.selected()
+assert.selected(selector)
+assert.not.selected(selector)
 ```
 
 
@@ -36,6 +36,73 @@ test.assert.selected('#checked_checkbox')
 
 */
 
-module.exports = function(/*dalek*/) {
-  // TODO: implement plugin assert.selected
+module.exports = function(dalek) {
+  var format = dalek.format;
+
+  // plugin meta data
+  var meta = {
+    // group to sort the plugin into
+    namespace: 'assert',
+    // name of the plugin
+    name: 'selected',
+    // allow calls like assert.attribute('.some-thing', 'attr-name', 'expected-value')
+    signature: ['selector'],
+    // list of properties that must be specified at the very least
+    required: ['selector'],
+    // mark this plugin capable of handling {match: "all"}
+    iterator: true,
+    // mark this plugin as capable of handling .not.attribute()
+    // this should be done only for assertions that have a boolean result (such as "visible", "hidden")
+    // for assertions that take a value for comparison, this should be handled via the expected function callback
+    invertable: true,
+  };
+
+  var handler = function(options) {
+    // the name invocations of this plugin will show up as
+    /*jshint laxbreak:true */
+    var label = 'Element ' + format.selector(options.selector)
+      + (options.inverted ? ' is not selected' : ' is selected');
+    /*jshint laxbreak:false */
+
+    // we're creating an assertion, give dalek that context
+    var handle = new dalek.Handle(label, dalek.Handle.ASSERTION, meta.name);
+
+    var isSelectedElements = function (elements) {
+      return dalek.Q.all(elements.map(function(element) {
+        return dalek.wd.isSelected(element);
+      }));
+    };
+
+    var verifySelectedStates = function(values) {
+      // walk results, abort with the first failure
+      // we always get an array returned, even if a single element was requested
+      values.some(function(value, index) {
+        if (Boolean(value) === Boolean(options.inverted)) {
+          // we caught an assertion failure. pass all the possible messages
+          // to the assertion instance and have *it* figure out what to show
+          handle.rejectWithMessage(index, options.message, value);
+          // end the loop, one failure is all we needed
+          return true;
+        }
+      });
+
+      // all tests passed, if handle was rejected,
+      // this call is ignored by the Promise
+      handle.resolveItems(values.length);
+    };
+
+    dalek.wd
+      // using dalek's simplified element matcher
+      .matchElements(options)
+      // we dispatch our real test
+      .then(isSelectedElements, handle.reject)
+      .then(verifySelectedStates, handle.reject)
+      // instead of .done() we inform dalek when something went terribly wrong
+      .catch(dalek.catch);
+
+    return handle;
+  };
+
+  // register plugin
+  dalek.registerPlugin(meta, handler);
 };
