481e58cf3e8d97616d320e5e57f068b9cfa22eed
Rodney Rehm <mail+github@rodneyrehm.de>
2015-04-25 20:32:37 +0200

    feature(example): some more fun

--- example/tests/wd-test.js
+++ example/tests/wd-test.js
@@ -9,8 +9,27 @@ module.exports = function(dalek) {
         browser.open('http://dalekjs.com/guineapig/'),
         // verify we weren't redirected
         assert.url('http://dalekjs.com/guineapig/'),
-        // click on the first link found in the document
-        action.click('a'),
+
+        // testing boolean assertion, click action
+        assert.not.selected('#unchecked_checkbox'),
+        action.click('unchecked_checkbox'.id),
+        assert.selected('#unchecked_checkbox'),
+
+        // // testing failure
+        // function deineMudda() {
+        //   return dalek.wd.execute(function() {
+        //     var el = document.getElementById('top-anchor');
+        //     el.style.display = 'none';
+        //   }, []);
+        // },
+        // action.click('top-anchor'.id),
+
+        // see if a boolean assertion can run on a set
+        assert.selected({
+          selector: '#checked_checkbox, #unchecked_checkbox',
+          match: 'all',
+        }),
+
         // make sure the name attribute has the correct value
         assert.attribute({
           selector: '#unchecked_checkbox',
